ed430625a7caa6230f6c611c8d9e0909ec3731da
MarkoSt0 <markostevic2003@gmail.com>
2026-04-06 13:48:54 +0200

    Dokumentovani BibliotekaInterfejs i Biblioteka

--- src/biblioteka/Biblioteka.java
+++ src/biblioteka/Biblioteka.java
@@ -6,6 +6,11 @@ import java.util.List;
 
 import biblioteka.interfejs.BibliotekaInterface;
 
+/**
+ * Implementira BibliotekaInterfejs preko interne liste sa knjigama
+ * @author Marko Stevic
+ *
+ */
 public class Biblioteka implements BibliotekaInterface {
 	
 	private List<Knjiga> knjige = new ArrayList<>();
@@ -34,6 +39,20 @@ public class Biblioteka implements BibliotekaInterface {
 		return knjige;
 	}
 
+	/**
+	 * Pretrazuje biblioteku i vraca sve knjige koje imaju uneti deo naslova.
+	 * 
+	 * Implementirana je pretraga <b>samo preko naslova</b>, a ne preko ostalih kriterijuma
+	 * 
+	 * @param autor Jedan od autora knjige.
+	 * @param isbn tacan isbn broj knjige.
+	 * @param naslov Deo naslova knjige. Ne mora se unositi ceo naslov.
+	 * @param izdavac Deo naziva izdavaca. Ne mora se unositi ceo naziv.
+	 * @return Lista sa svim knjigama koje odgovaraju kriterijumima ili praznu listu ako nijedna knjiga
+	 * ne odgovara.
+	 * @throws java.lang.IllegalArgumentException ako nije unet nijedan kriterijum pretrage,
+	 * odnosno ako su svi null/nula.
+	 */
 	@Override
 	public List<Knjiga> pronadjiKnjigu(Autor autor, long isbn, String naslov, String izdavac) {
 		if(autor == null && isbn <= 0 && naslov == null && izdavac == null)
